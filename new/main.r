2023ed9b71d82ccc35d5bbaa655ddf4b3c65245b
Artyom Lyan <avn8068@gmail.com>
2016-02-14 16:37:49 +0400

    3dim matrix is almost complete

--- new/main.r
+++ new/main.r
@@ -2,54 +2,61 @@ library(png)
 
 cat("started")
 
-img <- readPNG(native=TRUE, "/home/dodler/Documents/hist[R]/new/img/source.png")
-
-mean(img)
+img <- readPNG(native=TRUE, "/home/dodler/Documents/hist[R]/srw/new/img/source.png")
 d <- dim(img)
-
 print("image loaded")
 
 height = d[1]
 width = d[2]
 
-data = numeric(length = height * width)
+h <- hist(img)
 
-print ("calculating histogram")
+print (paste("width:", width))
+print (paste("height:", height))
 
-cnt = 1
-for (i in 1:height){
-  for (j in 1:width){
-    data[cnt] <- img[i,j]
-    cnt <- cnt + 1
-  }
+#function returns image intensity at points i,j
+image_intensity <- function (i,j){
+  return (img[i,j])
 }
 
-hist(data)
-
-print (width)
-print (height)
-
-print ("allocating mem")
-data_3 = array(dim = c(width, height, width*height,3))
-
-print ("started processing 3dim hst")
-
-cnt = 1
-for (i in 1:height){
-  for(j in 1:width){
-    
-    for(n in 1:width){
-      for(m in 1:height){
-        data_3[i][j][cnt][1] <- img[i][j]
-        data_3[i][j][cnt][2] <- img[i][j+m]
-        data_3[i][j][cnt][3] <- img[i][j+n]
-        cnt <- cnt + 1
-      }
-    }
-    
-  }
+#this function show if image has intensity levels i and j
+# at points m1,m2
+# and m1 + d_m1, m2+d_m2
+indicator <- function(i,j, m1, m2, d_m1, d_m2){
+  if (m1+d_m1 > height) return (0)
+  if (m2 + d_m2 > width) return (0)
+  
+  return (img[m1,m2] == i && img[m1+d_m1, m2+d_m2]==j)
+  #return (0)
 }
 
-print (dim(data_3))
+sum_indicator <- function(m1,m2){
+  i <- img[m1,m2]
+  
+  j_indexes <- expand.grid(1:height, 1:width)
+  
+  d_m1 <- unlist(j_indexes['Var1'])
+  d_m2 <- unlist(j_indexes['Var2'])
+
+  j <- mapply(image_intensity, d_m1, d_m2)
+  
+  i <- rep(i, width*height)
+  
+  #print("lengths")
+  #print (length(i))
+  #print (length(j))
+  #print (length(m1))
+  #print (length(m2))
+  #print (length(d_m1))
+  #print (length(d_m2))
+  
+  return (sum(mapply(indicator, i,j,m1,m2,d_m1, d_m2)))
+}
+
+pic_coords <- expand.grid(1:height, 1:width)
+
+print("running mapply to sum_indicator")
+data_3d <- mapply(FUN=sum_indicator, unlist(pic_coords['Var1']), unlist(pic_coords['Var2']))
+hist(data_3d)
 print ("finished")
 
